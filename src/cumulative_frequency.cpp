7486d7295144998b5b088b2b73de2ef373945ca4
Allan A. Zea <allan.zea@ciens.ucv.ve>
2019-12-08 16:48:02 +0100

    updated cumulative_frequency

--- src/cumulative_frequency.cpp
+++ src/cumulative_frequency.cpp
@@ -1,20 +1,28 @@
 #include <Rcpp.h>
+#include <utility>
 #include <algorithm>
 
 // [[Rcpp::export]]
 // Function cumulative_frequency
-Rcpp::NumericVector cumulative_frequency(Rcpp::NumericVector& M) {
-  Rcpp::NumericVector freq(M.size(), 0);
-  if (freq.size() == 1) {
-    freq[0] = 1;
+Rcpp::List cumulative_frequency(Rcpp::NumericVector& magnitudes) {
+  Rcpp::List cumulative_frequencies; 
+  Rcpp::NumericVector frequencies(magnitudes.size(), 0);
+  
+  if (frequencies.size() == 1) {
+    frequencies[0] = 1;
   } else {
-    std::sort(freq.begin(), freq.end());
-    freq[freq.size()-1] = 1;
-    for (int k = freq.size()-2; k >= 0; k--) {
-      freq[k] = freq[k+1] + 1;
+    std::sort(magnitudes.begin(), magnitudes.end());
+    std::sort(frequencies.begin(), frequencies.end());
+    frequencies[frequencies.size()-1] = 1;
+    for (int k = frequencies.size()-2; k >= 0; k--) {
+      frequencies[k] = frequencies[k+1] + 1;
     }
   }
-  return freq;
+  
+  cumulative_frequencies = Rcpp::List::create( Rcpp::Named("magnitude") = magnitudes, 
+                                               Rcpp::Named("count") = frequencies );
+  
+  return cumulative_frequencies;
 }
 
 
